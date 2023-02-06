6fe0f8f93b29fbdb578d3627e9bea6d88cced7ec
Felipe Cardozo <dafelcardozo@gmail.com>
2023-02-06 16:57:19 -0500

    Removed.

--- crud.py
+++ crud.py
@@ -4,31 +4,7 @@ import schemas
 from models import Blacklisted
 from datetime import datetime, timedelta
 
-"""
-def is_blacklisted(db: Session, email: str):
-    query = db.query(
-        Blacklisted.reason, Blacklisted.email,
-        func.count(Blacklisted.reason),
-        func.count(distinct(Blacklisted.game_id))
-    ).group_by(Blacklisted.reason, Blacklisted.email).having(Blacklisted.email == email)\
-    .order_by(func.count(Blacklisted.reason))
 
-    most_common_reason, _, count, count2 = query.first()
-    all_times_report = dict(most_common_reason=most_common_reason, most_common_reason_count=count, games_reported=count2)
- 
-    query = db.query(
-        Blacklisted.reason, Blacklisted.email,
-        func.count(Blacklisted.reason),
-        func.count(distinct(Blacklisted.game_id))
-    )\
-        .filter(Blacklisted.date >= (datetime.now() - timedelta(days=3)) )\
-        .group_by(Blacklisted.reason, Blacklisted.email).having(Blacklisted.email == email)\
-        .order_by(func.count(Blacklisted.reason))
-    most_common_reason, _, count, count2 = query.first()
-    last_90_days_report = dict(most_common_reason=most_common_reason, most_common_reason_count=count, games_reported=count2)
-    all_data = db.query(Blacklisted).filter(Blacklisted.email == email).all()
-    return dict(aggregates=dict(last_90_days=last_90_days_report, all_times_report=all_times_report), all_data=all_data)
-"""
 def is_blacklisted(db: Session, email: str) -> schemas.PlayerReport:
     query = db.query(
         Blacklisted.reason, Blacklisted.email,
